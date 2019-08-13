6461ab68288b64fa178d11c30e8ca35a651444d1
Brian <r847692@gmail.com>
2019-08-13 15:27:02 -0700

    Refactored Pedometer implementation to use Observer pattern.

--- app/src/main/java/com/example/fitnesswizards/MainActivity.java
+++ app/src/main/java/com/example/fitnesswizards/MainActivity.java
@@ -10,64 +10,43 @@ import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
-    private TextView stepsTakenText;
+    private TextView stepsSinceOpenedText;
     private TextView totalStepsTakenText;
 
-    //Sensor
-    private SensorManager sensorManager;
-    private Sensor sensor;
+    private Pedometer pedometer;
 
-    //Step counter variable
-    private int steps = 0;
-    private int stepsOffset = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //Instatiate step counter sensor
-        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-
-
-
         //Get layout elements
-        stepsTakenText = findViewById(R.id.main_opened_steps_taken);
+        stepsSinceOpenedText = findViewById(R.id.main_opened_steps_taken);
         totalStepsTakenText = findViewById(R.id.main_steps_taken);
 
-        //convert steps to string
-        stepsTakenText.setText("0");
+        stepsSinceOpenedText.setText("0");
+
+        //Create Pedometer object
+        pedometer = new Pedometer(this);
+        //Setup listener for Pedometer object
+        pedometer.setPodometerListener(new Pedometer.PedometerListener(){
+            @Override
+            public void onSensorChanged(int stepsSinceOpened, int stepsTotal) {
+                stepsSinceOpenedText.setText(String.valueOf(stepsSinceOpened));
+                totalStepsTakenText.setText(String.valueOf(stepsTotal));
+            }
+        });
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, sensor, sensorManager.SENSOR_DELAY_NORMAL);
+        pedometer.registerSensorListener();
     }
 
-    @Override
-    public void onSensorChanged (SensorEvent e){
 
-        Log.d(TAG, "onSensorChanged: ");
-        if (stepsOffset == 0) {
-            // initial value
-            stepsOffset = (int)e.values[0];
-        }
-
-        //set steps since app opened
-        stepsTakenText.setText(String.valueOf((int)e.values[0] - stepsOffset));
-
-        //set total steps
-        totalStepsTakenText.setText(String.valueOf((int)e.values[0]));
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int accuracy) {
-
-    }
 }
--- app/src/main/java/com/example/fitnesswizards/Pedometer.java
+++ app/src/main/java/com/example/fitnesswizards/Pedometer.java
@@ -1,4 +1,68 @@
 package com.example.fitnesswizards;
 
-public class Pedometer {
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.util.Log;
+
+public class Pedometer implements SensorEventListener {
+    private Context context;
+    //Sensor
+    private SensorManager sensorManager;
+    private Sensor sensor;
+
+    //Custom listener
+    private PedometerListener listener;
+
+    //Step counter variable
+    private int stepsOffset = 0;
+    int stepsSinceOpened;
+    int stepsTotal;
+
+    public Pedometer(Context context){
+        this.context = context;
+        //Instatiate step counter sensor
+        sensorManager = (SensorManager) this.context.getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+
+        //Instatiate listener
+        listener = null;
+    }
+
+    @Override
+    public void onSensorChanged (SensorEvent e){
+        if (stepsOffset == 0) {
+            // initial value
+            stepsOffset = (int)e.values[0];
+        }
+
+        //set steps since app opened
+        stepsSinceOpened = (int)e.values[0] - stepsOffset;
+
+        //set total steps
+        stepsTotal = (int)e.values[0];
+
+        //Trigger PodometerListener
+        listener.onSensorChanged(stepsSinceOpened, stepsTotal);
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+    void registerSensorListener(){
+        sensorManager.registerListener(this, sensor, sensorManager.SENSOR_DELAY_NORMAL);
+    }
+
+    public interface PedometerListener{
+        //When onSesnsorChange event occurs pass data up the chain to the main activity
+        void onSensorChanged(int stepsSinceOpened, int stepsTotal);
+    }
+
+    public void setPodometerListener(PedometerListener listener){
+        this.listener = listener;
+    }
 }
