143bb8ee5204a550c6039e3e2f55795d01464e62
Brian Stearns <r847692@gmail.com>
2020-05-03 18:39:00 -0700

    Started MainMenu activity.

---
+++ app/src/main/java/com/example/fitnesswizards/views/MainMenuActivity.java
@@ -0,0 +1,38 @@
+package com.example.fitnesswizards.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.fitnesswizards.MainActivity;
+import com.example.fitnesswizards.R;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu2);
+
+        Button createButton = findViewById(R.id.create_button);
+        createButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenuActivity.this, CharacterCreationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button loadButton = findViewById(R.id.load_button);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainMenuActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
