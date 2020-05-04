c8a75d3d611d68f318239deca84696eb4ca3467e
Brian Stearns <r847692@gmail.com>
2020-05-03 19:02:32 -0700

    Started CharacterCreationActivity.

--- app/src/main/java/com/example/fitnesswizards/views/CharacterCreationActivity.java
+++ app/src/main/java/com/example/fitnesswizards/views/CharacterCreationActivity.java
@@ -2,7 +2,10 @@ package com.example.fitnesswizards.views;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.fitnesswizards.R;
 
@@ -13,7 +16,13 @@ public class CharacterCreationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_character_creation);
 
-        //Set layout elements
-
+        Button loadButton = findViewById(R.id.cancel_button);
+        loadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(CharacterCreationActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
